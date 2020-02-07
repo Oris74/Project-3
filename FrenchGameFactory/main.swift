5634561ad97df7e70599c101f06786a95a97b93e
Laurent DEBEAUJON <Laurent.DEBEAUJON@gmail.com>
2020-02-07 11:54:08 +0100

    fix initialisation / methode & properties

--- FrenchGameFactory/main.swift
+++ FrenchGameFactory/main.swift
@@ -18,29 +18,18 @@ class Game {
     var player2: Player
     var nbOfTurn: Int = 0
     init() {
-        self.player1 = requestPlayerName(defaultName: "Joueur 1")
-        self.player2 = requestPlayerName(defaultName: "Joueur 2")
-        self.playerTurn = defineWhoStart()
+        self.player1 = Player(name: "Joueur 1")
+        self.player2 = Player(name: "Joueur 2")
+        self.playerTurn = self.player1
     }
     
-    //********* function requestPlayerName  **************
-    
-    func requestPlayerName(defaultName: String) ->Player {
-        print ("\n \(defaultName) : Saisisez votre nom (\(defaultName)): " )
-        if let playerName = readLine()  {
-             return Player(name: playerName)
-        }
-        else {
-             return Player(name: defaultName)
-            }
-    }
-    
+ 
      //********* function defineWhoStart()  **************
     
     func defineWhoStart() -> Player {
         let randomPlayer = Int.random(in:1...2)
         if randomPlayer == 1 {
-            return player1
+            return self.player1
         }
         return player2
     }
@@ -61,10 +50,10 @@ class Game {
 
 class Player: Teams {
     var name: String
-    var playerTeam: Teams
-
+    var playerTeam: Teams?
     init(name: String) {
         self.name = name
+        super.init()
         self.playerTeam = defineTeamToPlayer()
     }
     func defineTeamToPlayer() ->Teams {
@@ -73,6 +62,18 @@ class Player: Teams {
         return myTeam
     }
     
+    //********* function requestPlayerName  **************
+     
+     func requestPlayerName(defaultName: String) ->Player {
+         print ("\n \(defaultName) : Saisisez votre nom (\(defaultName)): " )
+         if let playerName = readLine()  {
+              return Player(name: playerName)
+         }
+         else {
+              return Player(name: defaultName)
+             }
+     }
+     
 }
 
 //****************************************************************
@@ -81,7 +82,7 @@ class Player: Teams {
 //***************************************************************
 
 class Teams {
-    var listOfCharacters: [Characters]
+    var listOfCharacters: [Characters] = []
     let nbOfCharacters = 3
     
     init() {
@@ -93,15 +94,16 @@ class Teams {
     
     func addNewCharacter() {
        let newFighter = Characters()
-            listOfCharacters.append(newFighter)
-            print("\(newFighter.fighterSelected.keys) vient de rejointe l'équipe avec \(newFighter.fighterSelected.values)!")
+       listOfCharacters.append(newFighter)
+        
+        print("\(newFighter.fighterSelected) vient de rejointe l'équipe avec \(newFighter.maxLifePoints)!")
       }
       
     
     func DisplayTeam() {
         print("\nTon equipe est constituée de \(listOfCharacters.count)")
         for teams in listOfCharacters {
-            print("\n\(teams.fighterSelected.keys) dispose de \(teams.fighterSelected.values) points de vie !")
+            print("\n\(teams.fighterSelected) dispose de \(teams.currentLifePoints) points de vie !")
         }
     }
 }
@@ -112,7 +114,7 @@ class Teams {
     //*****************************************************************
 
 class Characters {
-    var fighterSelected: [String: Int]?
+    var fighterSelected: String
     var weaponUsed: Weapons
     var currentLifePoints: Int
     let maxLifePoints: Int
@@ -120,12 +122,20 @@ class Characters {
     
     // [String:Int] => name : Point of life
     init() {
-        self.fighterSelected =
+        if let fighterFound = fighter.randomElement() {
+            self.fighterSelected = fighterFound.key
+            self.maxLifePoints = fighterFound.value
+            
+        
+        self.currentLifePoints = maxLifePoints
+        self.weaponUsed = Weapons()
+        }
     }
-    func selectCharacter() {
-        if (let mYFighter = fighter.randomElement() as?
+    /*func selectCharacter() -> fighter {
+        if let myFighter = fighter.randomElement(){
+        return myFighter
+        }*/
     }
-}
     //****************************************************************
     //***  Class Weapons
     //***  define caracteristics of weapons affected to fighters
@@ -136,6 +146,10 @@ class Weapons {
     let damage: Int
     let listOfWeapons: [String:Int] =  ["Sabre":10,"Hache":8,"Couteau":3,"Baton":1,"Epée":8,"Masse":5,"Arc":12,"Katana":11,"Machette":8,"Dague":3,"Poignard":3,"Fléau":5, "Arbalète":15]
     init () {
+        if let weapon = listOfWeapons.randomElement() {
+            self.name = weapon.key
+            self.damage = weapon.value
+        }
         
     }
     func CreateWeapon() {
@@ -150,6 +164,9 @@ class Weapons {
     
 class Chest {
     var itemInChest: Weapons
+    init(){
+        
+    }
     func selectRandomItem() {
         
     }
